2e7c5e2dfd750271423557b58103ea64db88740a
Aidan Wood <aidanrbwood@gmail.com>
2020-05-12 02:22:41 -0400

    -Altered cmakelists and UI code to allow for the project to be built with or without a UI - Cleaned up some imgui_demo code - Added a README to instruct on building

--- ui.cpp
+++ ui.cpp
@@ -1,21 +1,26 @@
+#ifndef NO_UI
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 #include "imgui/imgui.h"
+#endif
+
+#include <iostream>
 #include "DominoInput.h"
 #include "DominoSolver.h"
 
+#ifndef NO_UI
 static bool show_demo_window = false;
 static bool show_another_window = true;
 static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-void my_display_code();
+#endif
 
 int main(int argc, char** argv){
+#ifndef NO_UI
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -56,16 +61,15 @@ int main(int argc, char** argv){
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-		if (show_demo_window)
-			ImGui::ShowDemoWindow(&show_demo_window);
 		if (show_another_window)
 		{
 			ImGui::SetNextWindowPos(ImVec2(0,0));
 			ImGui::SetNextWindowSize(ImVec2(800,600));
-			ImGui::Begin("Mexican Train Solver", &show_another_window);	
+			ImGui::Begin("Mexican Train Solver", &show_another_window);
+			ImGui::Text("Press ESC to exit");	
 			ImGui::InputText("Enter starting tile", startDomino, IM_ARRAYSIZE(startDomino), ImGuiInputTextFlags_CharsDecimal);
 			ImGui::InputText("Enter tile range", tileRange, IM_ARRAYSIZE(tileRange), ImGuiInputTextFlags_CharsDecimal);
-			ImGui::InputTextMultiline("Enter your dominos\none domino per line in format:\n<domino1num1> <domino1num2>\n<domino2num1> <domino2num2>\n...", dominos, IM_ARRAYSIZE(dominos));
+			ImGui::InputTextMultiline("Enter your dominos\none domino per line\nnumbers space separated, ex:\n10 1\n2 3\n...", dominos, IM_ARRAYSIZE(dominos));
 			if (ImGui::Button("Solve"))
 			{
 				if (input.input_dominos_char(dominos, 0) && input.input_domino_range_char(tileRange, 0) && input.input_starting_domino_char(startDomino, 0))
@@ -99,14 +103,24 @@ int main(int argc, char** argv){
 	glfwDestroyWindow(window);
 	glfwTerminate();
 
+#else
+	DominoInput in;
+	// when not using UI need try catch due to use of fstream functions to take input
+	try {
+		in.input_starting_domino();
+		in.input_domino_range();
+		in.input_dominos();
+		DominoSolver solve(in.starting_domino, in.domino_range, in.dominos);
+
+		solve.print_solved_train();
+	} catch (const char* e) {
+		cerr << e << endl;
+	}
 	return 0;
+#endif
 }
 
-void my_display_code()
-{	
-	ImGui::ShowDemoWindow(&show_demo_window);
-}
-
+#ifndef NO_UI
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
@@ -119,5 +133,5 @@ void processInput(GLFWwindow* window)
 		glfwSetWindowShouldClose(window, true);
 	}
 }
-
+#endif
 
